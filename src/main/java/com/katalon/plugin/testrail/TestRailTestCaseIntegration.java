cda6f69bb05509abb6a8a6034cc82dc4bc24bfeb
son-pham-katalon <son.hpham@katalon.com>
2025-07-25 09:24:31 +0700

    Support mapping multiple TestRail ID

--- src/main/java/com/katalon/plugin/testrail/TestRailTestCaseIntegration.java
+++ src/main/java/com/katalon/plugin/testrail/TestRailTestCaseIntegration.java
@@ -2,12 +2,15 @@ package com.katalon.plugin.testrail;
 
 import com.katalon.platform.api.model.Integration;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class TestRailTestCaseIntegration implements Integration {
+    private static final String TESTRAIL_TESTCASE_DELIMITER = ",";
     private String testCaseId;
 
     public void setTestCaseId(String testCaseId) {
@@ -15,7 +18,21 @@ public class TestRailTestCaseIntegration implements Integration {
     }
 
     public String getTestCaseId() {
-        return testCaseId != null ? testCaseId.replaceAll("\\D", "") : null;
+        if (StringUtils.isBlank(testCaseId)) {
+            return StringUtils.EMPTY;
+        }
+
+        ArrayList<Long> results = new ArrayList<>();
+        String[] testRailTestCaseIds = testCaseId.split(TESTRAIL_TESTCASE_DELIMITER);
+        
+        for (String id : testRailTestCaseIds) {
+            String filtered = id.trim().replaceAll("\\D", "");
+            if (!filtered.isEmpty()) {
+                results.add(Long.parseLong(filtered));
+            }
+        }
+
+        return results.stream().map(String::valueOf).collect(Collectors.joining(TESTRAIL_TESTCASE_DELIMITER));
     }
 
     @Override
